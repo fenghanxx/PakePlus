c084d5f65a818df2ab9f7adf74f09993f2862c80
fenghanxx <95281930+fenghanxx@users.noreply.github.com>
2026-09-09 11:10:05 +0800

    update src file from pakeplus

---
+++ docs/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<script lang="ts">
+export default { name: "BlueNoteApp" };
+</script>
